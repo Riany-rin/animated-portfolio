a22a528388c53973a03ef8fca51fb3719b951461
Riany-rin <rianynurannisa@gmail.com>
2024-03-31 17:18:44 +0900

    Add image to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,16 @@ import "./navbar.scss"
 const Navbar = () => {
   return (
     <div className="navbar">
-        Navbar
+        {/* Sidebar*/}
+        <div className="wrapper">
+            <span>Lama Dev</span>
+            <div className="social">
+                <a href="#"><img src="/facebook.png" alt=""/></a>
+                <a href="#"><img src="/instagram.png" alt=""/></a>
+                <a href="#"><img src="/youtube.png" alt=""/></a>
+                <a href="#"><img src="/dribbble.png" alt=""/></a>
+            </div>
+        </div>
     </div>
   )
 }
