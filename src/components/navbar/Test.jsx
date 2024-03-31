1ec415fc3cc065c45f11b1be897f75370653a8e4
Riany-rin <rianynurannisa@gmail.com>
2024-03-31 22:02:14 +0900

    add duration to test box animation

--- src/components/navbar/Test.jsx
+++ src/components/navbar/Test.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion"
 const Test = () => {
   return (
     <div className="course">
-        <motion.div className="box" animate={{opacity:0}}></motion.div>
+        <motion.div className="box" animate={{opacity:0}} transition={{duration:2}}></motion.div>
     </div>
   )
 }
