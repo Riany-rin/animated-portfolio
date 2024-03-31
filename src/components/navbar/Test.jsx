c9df700c7dfacac30c86a4e84af5355e771fa172
Riany-rin <rianynurannisa@gmail.com>
2024-03-31 22:07:33 +0900

    add while hover animation

--- src/components/navbar/Test.jsx
+++ src/components/navbar/Test.jsx
@@ -5,9 +5,10 @@ const Test = () => {
     <div className="course">
         <motion.div 
             className="box"
-            initial={{opacity:0, scale:0.5}} 
-            animate={{opacity:1, scale:1}} 
-            transition={{duration:2, delay: 2}}
+            initial={{opacity:0.5, scale:0.5}} 
+            animate={{opacity:1, scale:1, x: 200}} 
+            transition={{duration:2,}}
+            whileHover={{opacity:1, scale:2}}
         ></motion.div>
     </div>
   )
