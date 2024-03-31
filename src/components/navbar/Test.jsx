62e3b16291ba8ca84b642778e454363f50892949
Riany-rin <rianynurannisa@gmail.com>
2024-03-31 22:05:18 +0900

    Add animation initial and delay

--- src/components/navbar/Test.jsx
+++ src/components/navbar/Test.jsx
@@ -3,7 +3,12 @@ import { motion } from "framer-motion"
 const Test = () => {
   return (
     <div className="course">
-        <motion.div className="box" animate={{opacity:0}} transition={{duration:2}}></motion.div>
+        <motion.div 
+            className="box"
+            initial={{opacity:0, scale:0.5}} 
+            animate={{opacity:1, scale:1}} 
+            transition={{duration:2, delay: 2}}
+        ></motion.div>
     </div>
   )
 }
