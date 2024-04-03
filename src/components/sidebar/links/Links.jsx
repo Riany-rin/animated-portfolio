2efcd41f4366508bb63aedbdfcce968ffe1d53f6
Riany-rin <rianynurannisa@gmail.com>
2024-04-03 15:11:47 +0900

    Add motion to links

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,14 +1,41 @@
+import { motion } from "framer-motion"
+
+const variants = {
+  open: {
+    transition: {
+      staggerChildren: 0.1,
+    }
+  },
+  closed: {
+    transition: {
+      staggerChildren: 0.05,
+      staggerDirection: -1,
+    }
+  },
+}
+
+const itemsVariants = {
+  open: {
+    y: 0,
+    opacity: 1
+  },
+  closed: {
+    y: 50,
+    opacity: 0
+  },
+}
+
 const Links = () => {
-const items = ["Homepage", "Services", "Portfolio", "Contact", "About"]
+  const items = ["Homepage", "Services", "Portfolio", "Contact", "About"]
 
   return (
-    <div className="links">
+    <motion.div className="links" variants={variants}>
       {items.map((item) => (
-        <a href={`#${item}`} key={item}>
+        <motion.a href={`#${item}`} key={item} variants={itemsVariants}>
           {item}
-        </a>
+        </motion.a>
       ))}
-    </div>
+    </motion.div>
   )
 }
 
