a79d08ee2266b71d48e3a32376e9fa10a0450c14
Riany-rin <rianynurannisa@gmail.com>
2024-04-03 15:38:17 +0900

    Add text to hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,6 +3,20 @@ import "./hero.scss"
 const Hero = () => {
   return (
     <div className="hero">
+        <div className="wrapper">
+            <div className="textContainer">
+                <h2>HARVEY TYLER</h2>
+                <h1>Web developer and UI designer</h1>
+                <div className="buttons">
+                    <button>See the Latest Works</button>
+                    <button>Contact Me</button>
+                </div>
+                <img src="/scroll.png" alt="scroll icon" />
+            </div>
+        </div>
+        <div className="slidingTextContainer">
+            Writer Content Creator Influencer
+        </div>
         <div className="imageContainer">
             <img src="/hero.png" alt="hero image"/>
         </div>
