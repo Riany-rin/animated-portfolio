7ca36d927684ef29f8e870f463146c47da59ee10
Riany-rin <rianynurannisa@gmail.com>
2024-04-03 18:22:54 +0900

    Add animation

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,18 +1,42 @@
 import "./hero.scss"
+import { motion } from "framer-motion"
+
+const textVariants = {
+    initial: {
+        x: -500,
+        opacity: 0
+    },
+    animate: {
+        x: 0,
+        opacity: 1,
+        transition: {
+            duration: 1,
+            staggerChildren: 0.1
+        }
+    },
+    scrollButton: {
+        opacity: 0,
+        y: 10,
+        transition: {
+            duration: 2,
+            repeat: Infinity
+        }
+    }
+}
 
 const Hero = () => {
   return (
     <div className="hero">
         <div className="wrapper">
-            <div className="textContainer">
-                <h2>HARVEY TYLER</h2>
-                <h1>Web developer and UI designer</h1>
-                <div className="buttons">
-                    <button>See the Latest Works</button>
-                    <button>Contact Me</button>
-                </div>
-                <img src="/scroll.png" alt="scroll icon" />
-            </div>
+            <motion.div className="textContainer" variants={textVariants} initial="initial" animate="animate">
+                <motion.h2 variants={textVariants}>HARVEY TYLER</motion.h2>
+                <motion.h1 variants={textVariants}>Web developer and UI designer</motion.h1>
+                <motion.div className="buttons" variants={textVariants}>
+                    <motion.button>See the Latest Works</motion.button>
+                    <motion.button>Contact Me</motion.button>
+                </motion.div>
+                <motion.img src="/scroll.png" alt="scroll icon" variants={textVariants} animate="scrollButton"/>
+            </motion.div>
         </div>
         <div className="slidingTextContainer">
             Writer Content Creator Influencer
