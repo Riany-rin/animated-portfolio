045a077ed178a28c95ec3725712dd913bd9b95c5
Riany-rin <rianynurannisa@gmail.com>
2024-04-20 22:22:43 +0900

    Add animation and style

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,4 +1,7 @@
+import { useScroll,motion, useSpring } from "framer-motion";
 import "./portfolio.scss"
+import { useRef } from "react";
+
 
 const items = [
     {
@@ -35,9 +38,26 @@ const Single = ({item}) =>{
         </section>
     )
 }
+
 const Portfolio = () => {
+    const ref = useRef()
+
+    const {scrollYProgress} = useScroll({
+        target: ref,
+        offset: ["end end", "start start"]
+    })
+
+    const scaleX = useSpring(scrollYProgress,{
+        stiffness:100,
+        damping: 30,
+    })
+
   return (
     <div className="portfolio">
+        <div className="progress">
+            <h1>Featured Works</h1>
+            <motion.div style={{scaleX}} className="progressBar"></motion.div>
+        </div>
         {items.map(item=>(
             <Single item={item} key={item.id}/>
         ))}
