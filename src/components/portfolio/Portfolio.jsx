116019e469e5fb7a4db7f4bf9b559d9159ffc812
Riany-rin <rianynurannisa@gmail.com>
2024-04-20 22:46:36 +0900

    Styling and add animation

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,4 +1,4 @@
-import { useScroll,motion, useSpring } from "framer-motion";
+import { useScroll,motion, useSpring, useTransform } from "framer-motion";
 import "./portfolio.scss"
 import { useRef } from "react";
 
@@ -33,14 +33,26 @@ const items = [
 
 const Single = ({item}) =>{
     const ref = useRef()
+
+    const {scrollYProgress} = useScroll({
+        target: ref,
+        // offset: ["start start", "end start"]
+    })
+
+    const y = useTransform(scrollYProgress,[0,1],[-300,300])
+
     return (
-        <section ref={ref}>
+        <section>
             <div className="container">
-                <img src={item.img} alt="" />
-                <div className="textContainer">
-                    <h2>{item.title}</h2>
-                    <p>{item.desc}</p>
-                    <button>See Demo</button>
+                <div className="wrapper">
+                    <div className="imageContainer" ref={ref}>
+                        <img src={item.img} alt="" />
+                    </div>
+                    <motion.div className="textContainer" style={{y}}>
+                        <h2>{item.title}</h2>
+                        <p>{item.desc}</p>
+                        <button>See Demo</button>
+                    </motion.div>
                 </div>
             </div>
         </section>
